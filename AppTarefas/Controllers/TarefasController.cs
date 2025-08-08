20ecefebfded7016126c8563ef87ef95151b3dcc
JoaoGaBriel4504 <joao.fuzel@aluno.senai.br>
2025-08-08 16:36:02 -0300

    Adiciona funcionalidade inicial de lista de tarefas
    - Atualiza `_Layout.cshtml` para definir idioma como "pt-br" e
      adiciona link no menu para a página "Lista de Tarefas".
    - Cria `TarefasController` com ação `Index` para exibir a view.
    - Adiciona classe `Tarefa` com propriedades para gerenciar tarefas.
    - Cria view `Index.cshtml` com título e cabeçalho "Lista de Tarefas".

---
+++ AppTarefas/Controllers/TarefasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppTarefas.Controllers
+{
+    public class TarefasController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        // Nome da Chave primaria deve ser Nome da Classe + "Id"
+        public int TarefaId { get; set; } // Id é a chave primaria
+        public string Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
